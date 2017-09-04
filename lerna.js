aac449beaf4c6547fa4581004f08b125b02edbbd
Vilius Lukosius <vilius@wix.com>
2017-09-04 07:40:00 +0300

    Added custom test task

--- lerna.js
+++ lerna.js
@@ -1,8 +1,15 @@
-const idea = require('lerna-script-tasks-idea'),
+const {loadPackages, iter, exec} = require('lerna-script'),
+  idea = require('lerna-script-tasks-idea'),
   modules = require('lerna-script-tasks-modules'),
   npmfix = require('lerna-script-tasks-npmfix'),
   dependencies = require('lerna-script-tasks-dependencies'),
   depcheck = require('lerna-script-tasks-depcheck');
+
+function test(log) {
+  return iter.forEach(loadPackages(), {log, build: 'test'})((lernaPackage, log) => {
+      return exec.script(lernaPackage, {log, silent: false})('test');
+  });
+}
 
 function sync(log) {
   return Promise.resolve()
@@ -12,6 +19,7 @@ function sync(log) {
 }
 
 module.exports = {
+  test,
   sync,
   idea: idea(),
   depcheck: depcheck(),
