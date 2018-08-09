fbb589213c921f4d621fce45cd2e2f81e136a065
Glib Alieksieiev <gliba@wix.com>
2018-08-09 18:44:03 +0300

    npmfix: sort dependencies lexicographically

--- tasks/npmfix/index.js
+++ tasks/npmfix/index.js
@@ -3,6 +3,18 @@ const {loadPackages, iter, fs} = require('lerna-script'),
   gitInfo = require('hosted-git-info'),
   {relative} = require('path')
 
+function sortByKey(obj) {
+  return Object.keys(obj)
+    .sort()
+    .reduce((acc, key) => ({...acc, [key]: obj[key]}), {})
+}
+
+function sortDependencies(deps) {
+  if (deps) {
+    return sortByKey(deps)
+  }
+}
+
 function npmfix({packages} = {}) {
   return log => {
     const lernaPackages = packages || loadPackages()
@@ -20,6 +32,9 @@ function npmfix({packages} = {}) {
           .then(packageJson => {
             const updated = Object.assign({}, packageJson, {
               homepage: moduleGitUrl,
+              dependencies: sortDependencies(packageJson.dependencies),
+              devDependencies: sortDependencies(packageJson.devDependencies),
+              peerDependencies: sortDependencies(packageJson.peerDependencies),
               repository: {
                 type: 'git',
                 url: moduleGitUrl
