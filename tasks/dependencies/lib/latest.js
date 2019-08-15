1e3f3c33118eec56b2cb0502fe2fe9fe6d6da141
eyalp <eyalp@wix.com>
2019-08-15 18:40:48 +0300

    added tests for auto select added support for silent execution

--- tasks/dependencies/lib/latest.js
+++ tasks/dependencies/lib/latest.js
@@ -3,23 +3,25 @@ const {exec} = require('child_process'),
   inquire = require('./inquire'),
   {fs, loadRootPackage} = require('lerna-script')
 
-function latestDependenciesTask({onInquire = () => ({}), addRange = ''} = {}) {
+function latestDependenciesTask({onInquire = () => ({}), addRange = '', fetch, silent} = {}) {
   return log => {
     log.info('latest', `checking for latest dependencies`)
     return checkForLatestDependencies(
       require(process.cwd() + '/lerna.json'),
       onInquire,
       addRange,
-      log
+      log,
+      fetch,
+      silent
     )
   }
 }
 
-function checkForLatestDependencies(lernaJson, onInquire, addRange, log) {
+function checkForLatestDependencies(lernaJson, onInquire, addRange, log, fetch, silent) {
   const {
     managedDependencies = {},
     managedPeerDependencies = {},
-    autoselect: {versionDiff, exclude} = {versionDiff: [], exclude: []}
+    autoselect: {versionDiff = [], exclude = []} = {versionDiff: [], exclude: []}
   } = lernaJson
 
   const depsList = Object.keys(cleanLatest(managedDependencies || {}))
@@ -28,69 +30,31 @@ function checkForLatestDependencies(lernaJson, onInquire, addRange, log) {
   const tracker = log.newItem('fetching', depsList.length + peerDepsList.length)
 
   const depsPromises = depsList.map(depName =>
-    fetchLatestVersion(depName, managedDependencies[depName], tracker)
+    fetchLatestVersion(depName, managedDependencies[depName], tracker, fetch)
   )
   const peerDepsPromises = peerDepsList.map(depName =>
-    fetchLatestVersion(depName, managedPeerDependencies[depName], tracker)
+    fetchLatestVersion(depName, managedPeerDependencies[depName], tracker, fetch)
   )
 
   return Promise.all([Promise.all(depsPromises), Promise.all(peerDepsPromises)]).then(
     ([deps, peerDeps]) => {
       tracker.finish()
       log.disableProgress()
-      const depsChoices = deps
-        .filter(
-          ({currentVersion, latestVersion}) => !satisfies(latestVersion, validRange(currentVersion))
-        )
-        .map(({name, currentVersion, latestVersion}) => {
-          return {
-            name: `${name}: ${currentVersion} -> ${latestVersion} (${diff(
-              currentVersion,
-              latestVersion
-            )})`,
-            value: {type: 'managedDependencies', name, latestVersion},
-            checked:
-              versionDiff.includes(diff(currentVersion, latestVersion)) && !exclude.includes(name)
-          }
-        })
 
-      const peerDepsChoices = peerDeps
-        .filter(
-          ({currentVersion, latestVersion}) => !satisfies(latestVersion, validRange(currentVersion))
-        )
-        .map(({name, currentVersion, latestVersion}) => {
-          return {
-            name: `${name}: ${currentVersion} -> ${latestVersion} (${diff(
-              currentVersion,
-              latestVersion
-            )})`,
-            value: {type: 'managedPeerDependencies', name, latestVersion},
-            checked:
-              versionDiff.includes(diff(currentVersion, latestVersion)) && !exclude.includes(name)
-          }
-        })
+      const depsChoices = createChoicesList(deps, 'managedDependencies', versionDiff, exclude)
+      const peerDepsChoices = createChoicesList(
+        peerDeps,
+        'managedPeerDependencies',
+        versionDiff,
+        exclude
+      )
 
-      const choiceGroups = []
-
-      if (depsChoices.length > 0) {
-        choiceGroups.push({name: 'dependencies/devDependencies', choices: depsChoices})
-      }
-      if (peerDepsChoices.length > 0) {
-        choiceGroups.push({name: 'peerDependencies', choices: peerDepsChoices})
-      }
-
-      if (choiceGroups.length > 0) {
-        onInquire()
-        return inquire({message: 'Updates found', choiceGroups}).then(answers => {
-          if (answers.length > 0) {
-            answers.forEach(
-              ({type, name, latestVersion}) =>
-                (lernaJson[type][name] = `${addRange}${latestVersion}`)
-            )
-            return fs.writeFile(loadRootPackage())('./lerna.json', lernaJson)
-          } else {
-            log.info('latest', `nothing selected, exiting...`)
-          }
+      if (depsChoices.length + peerDepsChoices.length > 0) {
+        let selections
+        if (silent) selections = findSelectedUpdates(depsChoices, peerDepsChoices)
+        else selections = inquireSelectedUpdates(depsChoices, peerDepsChoices, onInquire)
+        return selections.then(selectedUpdates => {
+          return writeSelectedUpdates(selectedUpdates, lernaJson, addRange, log)
         })
       } else {
         log.info('latest', `no updates found, exiting...`)
@@ -104,15 +68,70 @@ function cleanLatest(deps) {
   return deps
 }
 
-function fetchLatestVersion(name, version, logItem) {
+function createChoicesList(peerDeps, depType, versionDiff, exclude) {
+  return peerDeps
+    .filter(
+      ({currentVersion, latestVersion}) => !satisfies(latestVersion, validRange(currentVersion))
+    )
+    .map(({name, currentVersion, latestVersion}) => {
+      return {
+        name: `${name}: ${currentVersion} -> ${latestVersion} (${diff(
+          currentVersion,
+          latestVersion
+        )})`,
+        value: {type: depType, name, latestVersion},
+        checked:
+          versionDiff.includes(diff(currentVersion, latestVersion)) && !exclude.includes(name)
+      }
+    })
+}
+
+function inquireSelectedUpdates(depsChoices, peerDepsChoices, onInquire) {
+  const choiceGroups = []
+
+  if (depsChoices.length > 0) {
+    choiceGroups.push({name: 'dependencies/devDependencies', choices: depsChoices})
+  }
+  if (peerDepsChoices.length > 0) {
+    choiceGroups.push({name: 'peerDependencies', choices: peerDepsChoices})
+  }
+
+  onInquire()
+  return inquire({message: 'Updates found', choiceGroups})
+}
+
+function findSelectedUpdates(depsChoices, peerDepsChoices) {
+  function getSelectedValues(choices) {
+    return choices.filter(c => c.checked).map(c => c.value)
+  }
+
+  return Promise.resolve(getSelectedValues(depsChoices).concat(getSelectedValues(peerDepsChoices)))
+}
+
+function writeSelectedUpdates(selectedUpdates, lernaJson, addRange, log) {
+  if (selectedUpdates.length > 0) {
+    selectedUpdates.forEach(
+      ({type, name, latestVersion}) => (lernaJson[type][name] = `${addRange}${latestVersion}`)
+    )
+    return fs.writeFile(loadRootPackage())('./lerna.json', lernaJson)
+  } else {
+    log.info('latest', `nothing selected, exiting...`)
+  }
+}
+
+function fetchLatestVersionFromNpm(name) {
   return new Promise((resolve, reject) => {
     exec(`npm info ${name} dist-tags.latest`, (error, stdout) => {
-      logItem.completeWork(1)
-      error
-        ? reject(error)
-        : resolve({name, currentVersion: version, latestVersion: stdout.toString().trim('\n')})
+      error ? reject(error) : resolve(stdout.toString().trim('\n'))
     })
   })
+}
+
+function fetchLatestVersion(name, version, logItem, onFetch) {
+  const f = onFetch || fetchLatestVersionFromNpm
+  return f(name, version)
+    .then(result => ({name, currentVersion: version, latestVersion: result}))
+    .finally(() => logItem.completeWork(1))
 }
 
 module.exports.task = latestDependenciesTask
--- tasks/dependencies/test/latest.it.js
+++ tasks/dependencies/test/latest.it.js
@@ -93,6 +93,123 @@ describe('latest task', function() {
     return project.within(() => latest()(log))
   })
 
+  describe('auto select', () => {
+    it('should autoselect minor and patch updates', () => {
+      const {log, project} = setup({
+        managedDependencies: {
+          package1: '1.0.0'
+        },
+        managedPeerDependencies: {
+          package2: '2.0.0',
+          package3: '3.0.0'
+        },
+        autoselect: {
+          versionDiff: ['patch', 'minor']
+        }
+      })
+
+      const onInquire = () => bddStdin('\n')
+      const fetch = name => {
+        switch (name) {
+          case 'package1':
+            return Promise.resolve('1.0.1') //patch diff
+          case 'package2':
+            return Promise.resolve('2.1.1') //minor diff
+          case 'package3':
+            return Promise.resolve('4.1.1') //major diff
+        }
+      }
+
+      return project.within(ctx => {
+        return latest({onInquire, fetch})(log).then(() => {
+          const lernaJson = ctx.readJsonFile('lerna.json')
+
+          console.log(lernaJson)
+          expect(lernaJson.managedDependencies.package1).to.equal('1.0.1')
+          expect(lernaJson.managedPeerDependencies.package2).to.equal('2.1.1')
+          expect(lernaJson.managedPeerDependencies.package3).to.equal('3.0.0')
+        })
+      })
+    })
+
+    it('should autoselect major updates but exclude speicific packages', () => {
+      const {log, project} = setup({
+        managedDependencies: {
+          package1: '1.0.0'
+        },
+        managedPeerDependencies: {
+          package2: '2.0.0',
+          package3: '3.0.0'
+        },
+        autoselect: {
+          versionDiff: ['major'],
+          exclude: ['package3']
+        }
+      })
+
+      const onInquire = () => bddStdin('\n')
+      const fetch = name => {
+        switch (name) {
+          case 'package1':
+            return Promise.resolve('4.1.1') //major
+          case 'package2':
+            return Promise.resolve('2.1.2') //minor
+          case 'package3':
+            return Promise.resolve('4.0.9') //major
+        }
+      }
+
+      return project.within(ctx => {
+        return latest({onInquire, fetch})(log).then(() => {
+          const lernaJson = ctx.readJsonFile('lerna.json')
+
+          console.log(lernaJson)
+          expect(lernaJson.managedDependencies.package1).to.equal('4.1.1')
+          expect(lernaJson.managedPeerDependencies.package2).to.equal('2.0.0')
+          expect(lernaJson.managedPeerDependencies.package3).to.equal('3.0.0')
+        })
+      })
+    })
+
+    it('should respect silent flag', () => {
+      const {log, project} = setup({
+        managedDependencies: {
+          package1: '1.0.0'
+        },
+        managedPeerDependencies: {
+          package2: '2.0.0',
+          package3: '3.0.0'
+        },
+        autoselect: {
+          versionDiff: ['patch'],
+          exclude: ['package1', 'package3']
+        }
+      })
+
+      const fetch = name => {
+        switch (name) {
+          case 'package1':
+            return Promise.resolve('1.0.1') //patch
+          case 'package2':
+            return Promise.resolve('2.0.1') //patch
+          case 'package3':
+            return Promise.resolve('3.0.9') //patch
+        }
+      }
+
+      return project.within(ctx => {
+        return latest({fetch, silent: true})(log).then(() => {
+          const lernaJson = ctx.readJsonFile('lerna.json')
+
+          console.log(lernaJson)
+          expect(lernaJson.managedDependencies.package1).to.equal('1.0.0')
+          expect(lernaJson.managedPeerDependencies.package2).to.equal('2.0.1')
+          expect(lernaJson.managedPeerDependencies.package3).to.equal('3.0.0')
+        })
+      })
+    })
+  })
+
   function setup(lernaJsonOverrides = {}) {
     const log = loggerMock()
     const project = aLernaProject().lernaJson(lernaJsonOverrides)
